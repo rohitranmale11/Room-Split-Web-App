ece0d63c4645b84dcb4a43799b6a238384470c5c
rohitranmale11 <rohitranmale4@gmail.com>
2026-03-08 15:30:22 +0530

    Page File Isssue Fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,12 @@
-import { Suspense } from "react"
-import LoginPage from "./login-form"
+import { Suspense } from "react";
+import LoginPage from "./login-form";
+
+export const dynamic = "force-dynamic";
 
 export default function Page() {
   return (
     <Suspense fallback={<div className="p-6 text-center">Loading...</div>}>
       <LoginPage />
     </Suspense>
-  )
+  );
 }
